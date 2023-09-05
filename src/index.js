97a3c915a845825aabe0a81393b5f4f18a2cbde6
Kerem Deen <Kerem.deen@gmail.com>
2023-09-05 09:56:40 -0400

    catchrate added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const form = document.querySelector('#input-form')
 const topDisplay = document.querySelector('#topDisplay')
 const disPokeName = document.querySelector('#pokeName')
 const disPokeImg = document.querySelector('#displayPokemon')
+const pokeStat = document.querySelector('#pokemonStats')
 
 for(let i = 0; i < 5; i++){
     
@@ -14,19 +15,24 @@ for(let i = 0; i < 5; i++){
             img.setAttribute('topDisplay', (i+1))
             img.setAttribute('pokemonName', data.name)
             img.src = data.sprites.front_default
+            // pokeStat.textContent = `Catch rate :`
 
             topDisplay.prepend(img)
 
-            if(i=5 ){
-                disPokeName.textContent = img.getAttribute('pokemonName').toUpperCase()
-                disPokeImg.src = img.src
+            if(i=5){
+                // disPokeName.textContent = img.getAttribute('pokemonName').toUpperCase()
+                // disPokeImg.src = img.src
+                displayPokemon(data)
+                
             }
             // console.log(data)
             // console.log(parseInt(Math.random()*10000) % 151 + 1)
             
+            
             img.addEventListener('click', () => {
-                disPokeName.textContent = img.getAttribute('pokemonName').toUpperCase()
-                disPokeImg.src = img.src
+                // disPokeName.textContent = img.getAttribute('pokemonName').toUpperCase()
+                // disPokeImg.src = img.src
+                displayPokemon(data)
             })
             })
     
@@ -40,7 +46,25 @@ form.addEventListener('submit', e => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
         .then(resp => resp.json())
         .then(data => {
-            disPokeName.textContent = data.name.toUpperCase()
-            disPokeImg.src = data.sprites.front_default
+            // disPokeName.textContent = data.name.toUpperCase()
+            // disPokeImg.src = data.sprites.front_default
+            displayPokemon(data)
         })
 })
+
+function createPokemon(pokeObj) {
+
+}
+
+function displayPokemon(pokeObj) {
+    disPokeName.textContent = pokeObj.name.toUpperCase()
+    disPokeImg.src = pokeObj.sprites.front_default
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokeObj.name}`)
+        .then(respo => respo.json())
+        .then(species => {
+            pokeStat.textContent = `Catch rate: ${species.capture_rate} ${species.flavor_text_entries[5].flavor_text}`
+        })
+    disPokeImg.addEventListener('hover', () => {
+        toggl
+    })
+}
